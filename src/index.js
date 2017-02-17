6acc2fc590280576f818dd022eefe8a05023873a
Mark Mankarious <mz.mankarious@gmail.com>
2017-02-17 02:18:36 +0000

    Change Router to load after App

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, IndexRoute, hashHistory, browserHistory } from 'react-router';
 import App from './App';
 import SplashScreen from './SplashScreen';
 import Room from './Room';
 import './index.css';
+import { Router, Route, IndexRoute, hashHistory, browserHistory } from 'react-router';
 
 ReactDOM.render(
   <Router history={browserHistory}>
