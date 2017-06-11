dbaea4567bfb29df22e83d3b6db6e24acdba479b
Mark Mankarious <mz.mankarious@gmail.com>
2017-06-11 11:47:29 +0100

    Smoother animation when navigation buttons are opened

--- src/NavItem.js
+++ src/NavItem.js
@@ -11,43 +11,36 @@ const styles = {
     width: C.navItemSize,
     height: C.navItemSize,
     zIndex: 1,
+    overflow: 'hidden',
     backgroundColor: 'white'
   }, C.roundedCorners),
+  expanded: Utils.mergeStyles({
+    right: C.pagePadding * 0.5,
+    top: C.pagePadding * 0.5,
+    zIndex: 11
+  }, C.unroundedCorners),
   innerContent: {
     position: 'absolute',
     width: '100%',
     height: '100%',
     zIndex: 1,
-    overflow: 'hidden'
+    display: 'none'
   },
   content: {  // size is set dynamically
     position: 'absolute',
     left: C.panelPadding,
     top: C.panelPadding
   },
-  expanded: {
-    right: C.pagePadding * 0.5,
-    top: C.pagePadding * 0.5,
-    WebkitBorderRadius: 0,
-    MozBorderRadius: 0,
-    borderRadius: 0,
-    WebkitBoxShadow: 0,
-    MozBoxShadow: 0,
-    boxShadow: 0,
-    zIndex: 11
-  },
-  icon: Utils.mergeStyles({
-    position: 'absolute',
-    backgroundRepeat: 'no-repeat',
-    backgroundPosition: 'center',
-    backgroundSize: C.navItemSize * .5,
-    zIndex: 2,
-    cursor: 'pointer'
-  }, C.roundedCorners),
   navIcon: {
+    position: 'absolute',
     width: '100%',
     height: '100%',
+    zIndex: 2,
+    cursor: 'pointer',
     backgroundColor: C.color1,
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    backgroundSize: C.navItemSize * .5
   },
   closeIcon: {
     width: 20,
@@ -74,33 +67,29 @@ class NavItem extends Component {
 
     if (hasChanged['isExpanded']) {
       if (nextProps.commonProps.isExpanded) {
-        TweenMax.to(this.navIconEl, 0.4, {autoAlpha: 0, ease: Expo.easeInOut});
-        TweenMax.to(this.innerContent, 0.5, {borderRadius: 0, ease: Expo.easeInOut});
+        TweenMax.to(this.wrapperEl, 0.6, Utils.mergeStyles(styles.wrapper, styles.expanded, nextProps.commonProps.expandedStyle, {ease: Expo.easeInOut}))
+        TweenMax.set(this.navIconEl, {display: 'none', delay: 0.2});
+        TweenMax.set(this.innerContentEl, {display: 'inline', delay: 0.2});
       } else {
-        TweenMax.to(this.navIconEl, 0.2, {autoAlpha: 1, delay: 0.2});
-        TweenMax.to(this.innerContent, 0.5, {borderRadius: 1000, ease: Expo.easeInOut});
+        TweenMax.to(this.wrapperEl, 0.6, Utils.mergeStyles(styles.wrapper, nextProps.commonProps.posCss, {ease: Expo.easeInOut}));
+        TweenMax.set(this.navIconEl, {display: 'inline', delay: 0.4});
+        TweenMax.set(this.innerContentEl, {display: 'none', delay: 0.4});
       }
-    }
-
-    if (nextProps.commonProps.isExpanded) {
-      TweenMax.to(this.wrapperEl, 0.5, Utils.mergeStyles(styles.wrapper, styles.expanded, nextProps.commonProps.expandedStyle, {ease: Expo.easeInOut}))
-    } else {
-      TweenMax.to(this.wrapperEl, 0.5, Utils.mergeStyles(styles.wrapper, nextProps.commonProps.posCss, {ease: Expo.easeInOut}));
     }
   }
 
   render() {
     return (
       <div
-        ref={(el) => this.wrapperEl = el}
+        ref={ (el) => this.wrapperEl = el }
         style={ Utils.mergeStyles(styles.wrapper, C.enableGPU, this.props.commonProps.posCss) }>
 
         <Pulse 
           pulsate={this.props.commonProps.pulsate} />
 
         <div
-          style={ Utils.mergeStyles(styles.innerContent, C.roundedCorners) }
-          ref={(el) => this.innerContent = el}>
+          style={styles.innerContent}
+          ref={(el) => this.innerContentEl = el}>
 
           <div style={ Utils.mergeStyles(styles.content, {width: this.props.commonProps.expandedStyle.width - (C.panelPadding * 2), height: this.props.commonProps.expandedStyle.height - C.panelPadding * 2 }) } ref={el => this.content = el}>
             {this.props.children}
@@ -110,8 +99,8 @@ class NavItem extends Component {
         </div>
 
         <div
-          style={ Utils.mergeStyles(styles.icon, styles.navIcon, styles.circle, C.buttonShadow, Utils.genBgImgStyle(this.props.navIconUrl)) }
-          ref={(el) => this.navIconEl = el}
+          style={ Utils.mergeStyles(styles.navIcon, Utils.genBgImgStyle(this.props.navIconUrl)) }
+          ref={ (el) => this.navIconEl = el }
           onClick={(e, id) => this.onOpen(e, id)}>
         </div>
 
